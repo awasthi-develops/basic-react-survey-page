d56d6ef6b3527ccfc66843ffc291ca898e566b13
Shivam Awasthi <shivamawasthi@E470-PG013E2P.validuswealth.com>
2021-10-19 20:07:58 +0530

    fetching questions data through axios and rendering in class component

--- src/SurveyComponent.jsx
+++ src/SurveyComponent.jsx
@@ -7,20 +7,42 @@ import * as Survey from "survey-react";
 
 import "survey-react/modern.css";
 import "./index.css";
+import axios from "axios";
 
 Survey.StylesManager.applyTheme("modern");
 
 class SurveyComponent extends Component {
   constructor() {
     super();
-
+    this.state = {
+      data: []
+    };
+  }
+  componentDidMount() {
+    //keeping for future reference
+    // fetch('./question-bank.json'
+    //   , {
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'Accept': 'application/json'
+    //     }
+    //   }
+    // )
+    axios.get('./question-bank.json')
+      .then((j) => {
+        console.log('check', j.data);
+        return JSON.parse(JSON.stringify(j.data));
+      })
+      .then((questions) => {
+        console.log(questions);
+        this.setState({ data: questions });
+      });
   }
   render() {
 
 
 
-
-    const json = {
+    let baseSurvey = {
       title: "Fundamental Mathematics Quiz",
       showProgressBar: "bottom",
       showTimerPanel: "top",
@@ -28,49 +50,18 @@ class SurveyComponent extends Component {
       maxTimeToFinish: 25,
       firstPageIsStarted: true,
       startSurveyText: "Start Quiz",
-      pages: [
-        { questions: [{ type: "html", html: "You are about to start Test. <br/>You have 10 seconds for every page and 25 seconds for the whole quiz of 3 questions.<br/>Please click on <b>'Start Quiz'</b> button when you are ready." }] },
-        {
-          questions: [
-            {
-              type: "radiogroup",
-              name: "civilwar",
-              title: "What is 33*37?",
-              choices: ["1221", "1121", "2441", "4141"],
-              correctAnswer: "1221"
-            }
-          ]
-        },
-        {
-          questions: [
-            {
-              type: "radiogroup",
-              name: "libertyordeath",
-              title: "What is the GCD of 24,36,72?'",
-              choicesOrder: "random",
-              choices: ["12", "8", "72", "24"],
-              correctAnswer: "12"
-            }
-          ]
-        },
-        {
-          maxTimeToFinish: 15,
-          questions: [
-            {
-              type: "radiogroup",
-              name: "magnacarta",
-              title: "What comes next in this series 1,2,5,10,17,26,_?",
-              choicesOrder: "random",
-              choices: ["52", "29", "37", "31"],
-              correctAnswer: "37"
-            }
-          ]
-        }
-      ],
+      pages: [{
+        questions: [
+          {
+            type: "html",
+            html: "You are about to start Test. <br/>You have 10 seconds for every page and 25 seconds for the whole quiz of 3 questions.<br/>Please click on <b>'Start Quiz'</b> button when you are ready."
+          }
+        ]
+      }],
       completedHtml: "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
     };
-    const survey = new Survey.Model(json);
-
+    baseSurvey.pages.push(...this.state.data);
+    const survey = new Survey.Model(baseSurvey);
 
 
 
